707895d34143217bd683bf413887632c593dd012
Kirstin Megga Ramos <kirstinmegga.ramos@ph.softvision.global>
2022-06-11 17:25:16 +0800

    initial commit

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// First version of Singleton demo
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Assuming Singleton is created from employee class
+             * we refer to the GetInstance property from the Singleton class
+             */
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+            /*
+             * Assuming Singleton is created from student class
+             * we refer to the GetInstance property from the Singleton class
+             */
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+
+            Console.ReadLine();
+        }
+    }
+}
